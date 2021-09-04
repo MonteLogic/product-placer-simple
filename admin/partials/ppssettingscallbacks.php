0cc825a0049d6bfe1155a1bcbe37a1eadacaea71
Dennis Chavours <dchavours@gmail.com>
2021-09-04 00:24:24 -0500

    Added logic with mho-pp; todo change sunset to pps

--- admin/partials/ppssettingscallbacks.php
+++ admin/partials/ppssettingscallbacks.php
@@ -32,3 +32,45 @@
 			<p class="description">Write short product description</p>
 
             
+
+
+
+<input type="text" name="product_name" value="'.$productName.'" placeholder="Product Name" /> 
+
+
+
+<input type="text" name="user_description" value="'.$description.'" placeholder="Description" />
+			<p class="description">Write short product description</p>
+
+
+            <h1>PPS Options</h1>
+<?php settings_errors(); ?>
+
+<?php 
+	
+	$picture = esc_attr( get_option( 'profile_picture' ) );
+	$productName = esc_attr( get_option( 'product_name' ) );
+	$lastName = esc_attr( get_option( 'last_name' ) );
+	$fullName = $productName;
+	$description = esc_attr( get_option( 'user_description' ) );
+	
+?>
+
+<div class="sunset-sidebar-preview">
+	<div class="sunset-sidebar">
+		<div class="image-container">
+			<div id="profile-picture-preview" class="profile-picture" style="background-image: url(<?php print $picture; ?>);"></div>
+		</div>
+		<h1 class="sunset-username"><?php print $fullName; ?></h1>
+		<h2 class="sunset-description"><?php print $description; ?></h2>
+		<div class="icons-wrapper">
+			
+		</div>
+	</div>
+</div>
+
+<form method="post" action="options.php" class="sunset-general-form">
+	<?php settings_fields( 'sunset-settings-group' ); ?>
+	<?php do_settings_sections( 'alecaddd_sunset' ); ?>
+	<?php submit_button(); ?>
+</form>
